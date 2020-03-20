f14d873cfda5a897bcba30ab1d4c242636429e4d
Why-328 <1429101552@qq.com>
2020-03-20 19:51:31 +0800

    jump7

---
+++ jump7.py
@@ -0,0 +1,7 @@
+ = 0
+ while a < 100:
+        a += 1
+            if a % 10 == 7 or a % 7 == 0 or a // 7 == 10 or a == 78 or a == 79:
+                        continue
+                        print(a)
+
